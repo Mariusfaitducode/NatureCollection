08b8f92f7fa37be9e7de8d1830980b99402a93f9
SkateZen <mariusmdm123@gmail.com>
2023-05-05 23:46:06 +0200

    work on first page

--- app/src/main/java/fr/marscode/naturecollection/MainActivity.kt
+++ app/src/main/java/fr/marscode/naturecollection/MainActivity.kt
@@ -2,10 +2,17 @@ package fr.marscode.naturecollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.marscode.naturecollection.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Injecter le fragment dans notre boite (fragment container)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
